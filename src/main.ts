021e3ea1bf7c861fda1e5cdf3e4878536b7ee3f1
Lucas Gazzola <lucasgazzola1@outlook.com>
2025-09-03 15:09:15 -0300

    fixed port

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Habilita CORS para el frontend
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
